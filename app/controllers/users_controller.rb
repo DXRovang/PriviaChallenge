1fc0e9dfac32ea40632c73b1d68f87ce0283116c
Alexander Rovang <DXRovang@gmail.com>
2022-03-29 11:43:16 -0400

    adds button for update & link from index to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find_by(id: params[:id])
   end
 
   def update
-
+    @user = User.find_by(id: params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   def delete
