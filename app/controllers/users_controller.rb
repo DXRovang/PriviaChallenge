c58d0d4acd74e61e21aa2a454d7ebe0023cc1c31
Alexander Rovang <DXRovang@gmail.com>
2022-03-29 11:26:32 -0400

    adds user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,20 @@ class UsersController < ApplicationController
   end
 
   def new
-
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def show
-
+    @user = User.find_by(id: params[:id])
   end
 
   def edit
@@ -30,6 +35,13 @@ class UsersController < ApplicationController
   private
 
   def user_params
-
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :city,
+      :state,
+      :score
+    )
   end
 end
