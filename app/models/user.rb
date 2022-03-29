e34533ff7785c679f6ab5bd601deed50bfcab069
Alexander Rovang <DXRovang@gmail.com>
2022-03-29 13:12:48 -0400

    adds email syntax validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  include ActiveModel::Validations
   validates_presence_of :first_name, :last_name, :email, :city, :state, :score
-
+  validates_with EmailValidator
+  
   def full_name
     self.first_name + " " + self.last_name
   end
