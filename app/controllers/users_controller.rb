2e42135e029d4e7de446fee70332cf0d5f08e75a
Alexander Rovang <DXRovang@gmail.com>
2022-03-29 10:58:36 -0400

    adds empty actions to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+  def index
+
+  end
+
+  def new
+    
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def user_params
+
+  end
 end
