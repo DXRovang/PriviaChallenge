4c11ef571cb7872d899df64a207b3b21211db166
Alexander Rovang <DXRovang@gmail.com>
2022-03-29 11:11:27 -0400

    adds full_name method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  validates_presence_of 
-  :first_name, 
-  :last_name,
-  :email,
-  :city,
-  :state,
-  :score
+  validates_presence_of :first_name, :last_name, :email, :city, :state, :score
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
 end
