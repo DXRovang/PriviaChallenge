da01f64ab424cf5e15bd3f126636c7c27ae47b45
Alexander Rovang <DXRovang@gmail.com>
2022-03-30 14:41:39 -0400

    deletes extra code

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,40 +18,5 @@ RSpec.describe UsersController, :type => :controller do
     end
   end
 end
-# describe UsersController do
 
-#   describe "GET #index" do
-#     it "populates an array of users" do
-#       user = create(:user)
-#       get :index
-#       expect(User.all).to include(user)
-#     end
-#     it "renders the :index view" do
-#       get :index
-#       response.should render_template :index
-#     end
-#   end
-  
-#   describe "GET #show" do
 
-#     it "renders the :show template" do
-#       @user = FactoryBot.create(:user)
-#       params = { id: 123 }
-#       get :show, params
-#       expect(@user).to be(:user)
-#     end
-#   end
-  
-
-#   describe "GET #new" do
-
-#     it "renders the :new template" do
-#       get :new
-#       response.should render_template :new
-#     end
-#   end
-  
-
-  
-
-# end
