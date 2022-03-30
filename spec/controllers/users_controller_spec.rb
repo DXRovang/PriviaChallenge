fbf985509ea12abb4ae8faa865ec49949713898e
Alexander Rovang <DXRovang@gmail.com>
2022-03-30 19:02:06 -0400

    cleans up code

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
   describe "GET #index" do
+    let(:user) { create :user }
     it "responds successfully with an HTTP 200 status code" do
       get :index
       expect(response).to have_http_status(200)
     end
-    it "renders the index template" do
+    it "should return the index template" do
       get :index
       expect(response).to render_template("index")
     end
     it "populates an array of users" do
-      user = create(:user)
       get :index
       expect(User.all).to include(user)
     end
@@ -24,13 +24,24 @@ RSpec.describe UsersController, :type => :controller do
       get :show, params:  { id: user.id }
       expect(response).to have_http_status(200)
     end
+    it "should return the show template" do
+      get :show, params: { id: user.id }
+      expect(response).to render_template :show
+    end
     it "should return the user" do
       get :show, params: { id: user.id }
       expect(assigns(:user)).to eq user
     end
-    it "should return the users template" do
-      get :show, params: { id: user.id }
-      expect(response).to render_template :show
+  end
+
+  describe "GET #new" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+    it "should return the new template" do
+      get :new
+      expect(response).to render_template :new
     end
   end
 
@@ -40,15 +51,17 @@ RSpec.describe UsersController, :type => :controller do
       get :edit, params:  { id: user.id }
       expect(response).to have_http_status(200)
     end
-    it "should return the user" do
-      get :edit, params: { id: user.id }
-      expect(assigns(:user)).to eq user
-    end
     it "should return the edit template" do
       get :edit, params: { id: user.id }
       expect(response).to render_template :edit
     end
+    it "should return the user" do
+      get :edit, params: { id: user.id }
+      expect(assigns(:user)).to eq user
+    end
   end
+
+
 
 end
 
